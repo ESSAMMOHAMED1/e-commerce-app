1a496bfaef7580cc0b0f15eeea2dcc82b9af0c6e
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-12-09 12:35:26 -0800

    update

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,13 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import "../../css/Filter/Filter.css";
 const Filter = () => {
   return (
     <>
-    
-    
-    
-    </>
-  )
-}
+      <div className="filter-wrapper">
+        <h2 className="filter-title">Filter</h2>
+        <div className="number-product"> number of product </div>
+        <div className="filter-size">
+          <span> Filter </span>
+          <select className="filter-select">
+            <option value="ALL">all</option>
+            <option value="S">XL</option>
+            <option value="M" >M</option>
+            <option value="L">L</option>
+            <option value="XXL">XXL</option>
+            <option value="XXXL">XXXL</option>
 
-export default Filter
+          </select>
+        </div>
+        <div className="filter-size">
+          <span> Order </span>
+          <select className="filter-select">
+            <option value="latest">latest</option>
+            <option value="highest">highest</option>
+            <option value="lower" >lower</option>
+          </select>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Filter;
